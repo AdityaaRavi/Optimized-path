8d7f4b82ea196615af2c217f45586828f37bb46d
Adityaa Ravi <adityaaravi6@gmail.com>
2020-12-26 15:36:28 -0800

    Main class updated to reflect the changes done in ForestAlgorithm and to use the file name given for the output file if any

--- src/main.java
+++ src/main.java
@@ -11,16 +11,17 @@ public class Main{
     public static void main(String[] args){
         // get the board as a char[][] using the ingest class.
         char[][] board;
-
+        String outputPath = "data/output.txt";
         // use try/catch to use the file name given as a parameter or call the default constructor for the ingest class.
         try{
             board = (new IngestForest(args[0])).getBoard();
+            outputPath = args[1];
         }catch(IndexOutOfBoundsException e){
             board = (new IngestForest()).getBoard();
         }
         
         // Creating an object of the algorithm class
-        Algorithm forest = new ForestAlgorithm(board, 0, 0);
+        Algorithm forest = new ForestAlgorithm(board, 0, 0, 4);
         
         //run the algorithm
         forest.calculate(forest.getStartX(), forest.getStartY(), 0, new ArrayList<Integer[]>());
@@ -29,7 +30,7 @@ public class Main{
         //ArrayList<Integer[]> result = forest.getBestPath();
         
         //Create a new Show class object that prints out the path as both the (dx, dy) and on the map as well.
-        (new ForestShow("Output.txt")).formatAndPrint(forest, true);
+        (new ForestShow(outputPath)).formatAndPrint(forest, true);
 
 
 
