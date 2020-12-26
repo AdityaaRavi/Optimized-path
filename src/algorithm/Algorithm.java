f724f3b0cdff0e5af0ce6d6dc175d308b11c67d2
Adityaa Ravi <adityaaravi6@gmail.com>
2020-12-25 18:59:50 -0800

    timely save

--- src/algorithm/Algorithm.java
+++ src/algorithm/Algorithm.java
@@ -5,20 +5,25 @@ import java.util.ArrayList;
 public abstract class Algorithm {
     //class fields
     char[][] board;
+    int startX;
+    int startY;
     // current data
-    int current_num_occur;
-    ArrayList<Integer[]> curr_path;
+    int current_num_occur = 0;
+    ArrayList<Integer[]> curr_path = new ArrayList<Integer[]>();
     // data of best path
-    int best_num_occur;
-    ArrayList<Integer[]> best_path;
+    int best_num_occur = Integer.MAX_VALUE;
+    ArrayList<Integer[]> best_path = new ArrayList<Integer[]>();
     // constants
     final int DEAD_PATH = -1;
     final int PATH_FOUND = 1;
+    //possible movements that can be enacted in each iteration.
+    ArrayList<Integer[]> possibleMovements = getPossibleMovements();
 
     //constructor
-    public Algorithm(char[][] board){
+    public Algorithm(char[][] board, int startX, int startY){
         this.board = board;
-        
+        this.startX = startX;
+        this.startY = startY;
     }
     
     public int calculate(){
@@ -26,6 +31,9 @@ public abstract class Algorithm {
         return 0;
     }
 
-
+    //abstract method to get all possible movements in the current board being tested.
+    abstract ArrayList<Integer[]> getPossibleMovements();
+    //abstract method to check if the end condition is reached
+    abstract boolean reachedEnd(int currX, int currY);
 
 }
--- src/algorithm/ForestAlgorithm.java
+++ src/algorithm/ForestAlgorithm.java
@@ -1,10 +1,24 @@
 package algorithm;
 
-public class ForestAlgorithm extends Algorithm{
+import java.util.ArrayList;
 
-    public ForestAlgorithm(char[][] board) {
-        super(board);
+public class ForestAlgorithm extends Algorithm {
+
+    public ForestAlgorithm(char[][] board, int startX, int startY) {
+        super(board, startX, startY);
         // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    ArrayList<Integer[]> getPossibleMovements() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    boolean reachedEnd(int currX, int currY) {
+        // TODO Auto-generated method stub
+        return false;
     }
 
 }
