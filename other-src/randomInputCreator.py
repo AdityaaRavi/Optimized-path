5812d5381ed0da267215e8acc22dc7419bd7a191
Adityaa Ravi <adityaaravi6@gmail.com>
2020-12-25 17:03:25 -0800

    fill board with obstacle initial code done

--- other-src/randomInputCreator.py
+++ other-src/randomInputCreator.py
@@ -1,4 +1,5 @@
 # This program will make a random board based on the given parameters.
+import random
 
 file_name = input("Enter the file name of the result: ")
 w, h, num_trees = input("Enter the width, height and the number of trees you need each seperated by a space: ").split()
@@ -13,7 +14,21 @@ def fill_board(board, w, h):
         for j in range(0, h):
             board[i].append(empty_char)
 
+def print_board(board=board):
+    for row in board:
+        print(row)
+
 # testing the fill_board method
+print("Testing the fill empty char method")
 fill_board(board, w, h)
-for row in board:
-    print(row)
+print_board()
+
+def fill_obstacle(board, probability, w):
+    for i in range(0, w):
+        for char in board[i]:
+            char = random.choices([empty_char, tree_char], weights=[1-probability, probability], k=1)[0]
+
+#testing fill obstacle in array method
+print("Testing fill obstacle method: ")
+fill_obstacle(board, num_trees/w*h, w)
+print_board()
