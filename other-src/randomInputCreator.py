3f09c4e23db0d5102eb60c7d05054f7109c2aa5b
Adityaa Ravi <adityaaravi6@gmail.com>
2020-12-25 16:50:33 -0800

    fill board method inital code done

--- other-src/randomInputCreator.py
+++ other-src/randomInputCreator.py
@@ -5,4 +5,14 @@ w, h, num_trees = input("Enter the width, height and the number of trees you nee
 tree_char, empty_char = input("Enter the char to use for denoting the obstacle and the char to denote an empty space the same way: ").split()
 w, h, num_trees = int(w), int(h), int(num_trees)
 
+board = []
 
+def fill_board(board, w, h):
+    for i in range(0, w):
+        board.append([])
+        for j in range(0, h):
+            board[i].append(empty_char)
+
+# testing the fill_board method
+for row in board:
+    print(row)
