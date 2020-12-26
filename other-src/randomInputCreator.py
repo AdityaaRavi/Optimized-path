a88e8212bbf8a698557746d3fb3667db51f0013c
Adityaa Ravi <adityaaravi6@gmail.com>
2020-12-25 17:37:58 -0800

    full input generator program done

--- other-src/randomInputCreator.py
+++ other-src/randomInputCreator.py
@@ -2,16 +2,16 @@
 import random
 
 file_name = input("Enter the file name of the result: ")
-w, h, num_trees = 4, 4, 4 #input("Enter the width, height and the max number of trees you need each seperated by a space: ").split()
-tree_char, empty_char = '#', '=' #input("Enter the char to use for denoting the obstacle and the char to denote an empty space the same way: ").split()
+w, h, num_trees = input("Enter the width, height and the max number of trees you need each seperated by a space: ").split()
+tree_char, empty_char = input("Enter the char to use for denoting the obstacle and the char to denote an empty space the same way: ").split()
 w, h, num_trees = int(w), int(h), int(num_trees)
 
 board = []
 
 def fill_board(board, w, h):
-    for i in range(0, w):
+    for i in range(0, h):
         board.append([])
-        for j in range(0, h):
+        for j in range(0, w):
             board[i].append(empty_char)
 
 def print_board(board=board):
@@ -26,9 +26,9 @@ def print_board(board=board):
 def fill_obstacle(board, probability, w, h=h):
     curr_trees = 0
     probability *= 100
-    for i in range(0, w):
+    for i in range(0, h):
         board.append([])
-        for j in range(0, h):
+        for j in range(0, w):
             choice = random.choices([empty_char, tree_char], weights=[100-probability, probability], k=1)[0]
             #print("The choice picked is ", choice, " The probablity is:", probability)
             if(choice == tree_char):
@@ -46,12 +46,13 @@ print_board()
 def print_to_file(board, w, h, filename=file_name):
     file_handler = open(filename, "w")
     writeme = ""
-    for i in range(0, w):
-        for j in range(0, h):
+    for i in range(0, h):
+        for j in range(0, w):
             writeme += board[i][j] + " "
             print(board[i][j], end=" ")
         writeme += "\n"
         print()
+    file_handler.write(writeme)
     file_handler.close()
 
 # testing print to file method
