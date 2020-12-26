4fd2d7b40d62b9c291553d8b8ca8cab2f6aa72f4
Adityaa Ravi <adityaaravi6@gmail.com>
2020-12-25 17:09:33 -0800

    fill board with obstacle debug 2 -- trying out a fix for getting the correct probability value

--- other-src/randomInputCreator.py
+++ other-src/randomInputCreator.py
@@ -26,8 +26,8 @@ print_board()
 def fill_obstacle(board, probability, w):
     for i in range(0, w):
         for char in board[i]:
-            choice = random.choices([empty_char, tree_char], weights=[1-probability, probability], k=1)[0]
-            print("The choice picked is {choice}")
+            choice = random.choices([empty_char, tree_char], weights=[1-probability/10, probability/10], k=1)[0]
+            print("The choice picked is ", choice, " The probablity is:", probability)
             char = choice
 
 #testing fill obstacle in array method
