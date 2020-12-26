b471b897ccf54d62f57f01e545ff8b16eceb34cd
Adityaa Ravi <adityaaravi6@gmail.com>
2020-12-25 17:30:18 -0800

    print to file inital code done

--- other-src/randomInputCreator.py
+++ other-src/randomInputCreator.py
@@ -1,7 +1,7 @@
 # This program will make a random board based on the given parameters.
 import random
 
-#file_name = input("Enter the file name of the result: ")
+file_name = input("Enter the file name of the result: ")
 w, h, num_trees = 4, 4, 4 #input("Enter the width, height and the max number of trees you need each seperated by a space: ").split()
 tree_char, empty_char = '#', '=' #input("Enter the char to use for denoting the obstacle and the char to denote an empty space the same way: ").split()
 w, h, num_trees = int(w), int(h), int(num_trees)
@@ -42,3 +42,18 @@ def fill_obstacle(board, probability, w, h=h):
 print("Testing fill obstacle method: ")
 fill_obstacle(board, num_trees/(w*h), w, h)
 print_board()
+
+def print_to_file(board, w, h, filename=file_name):
+    file_handler = open(filename, "w")
+    writeme = ""
+    for i in range(0, w):
+        for j in range(0, h):
+            writeme += board[i][j] + " "
+            print(board[i][j], end=" ")
+        writeme += "\n"
+        print()
+    file_handler.close()
+
+# testing print to file method
+print("Testing print to file method: ")
+print_to_file(board, w, h, file_name)
